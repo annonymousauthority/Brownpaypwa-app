5be5062dc985ecd15ff1188e5db23662d2b7d2b7
Razeb Enyi <enyirazeb@gmail.com>
2021-09-29 08:22:10 +0100

    Fix padding

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="sidepanel">
     <div
-      class=" 
-       sidebar        
+      class="
+        sidebar
+        mx-auto
+        w-96
         py-8
         bg-white
         text-center
         border
         dark:bg-gray-900
         dark:border-gray-600
+        fixed
       "
     >
       <div class="flex flex-col items-center mt-6 lg:-ml-8">
@@ -37,9 +40,7 @@
           </div>
         </div>
         <div class="flex flex-col items-center lg:ml-32">
-          <div class="flex">
-            
-          </div>
+          <div class="flex"></div>
         </div>
       </div>
 
@@ -47,10 +48,7 @@
 
       <div class="flex flex-col justify-between flex-1 ml-0 xl:ml-4">
         <nav>
-          <router-link
-            to="/dashboard/main"
-            exact-active-class="current"
-          >
+          <router-link to="/dashboard/main" exact-active-class="current">
             <a
               class="
                 flex
@@ -85,10 +83,7 @@
               <span class="mx-4 text-center font-medium">Dashboard</span>
             </a>
           </router-link>
-          <router-link
-            to="/dashboard/transaction"
-           active-class="current"
-          >
+          <router-link to="/dashboard/transaction" active-class="current">
             <a
               class="
                 flex
@@ -133,117 +128,126 @@
             </a>
           </router-link>
 
-        <router-link to="/dashboard/wallet" active-class="current">
+          <router-link to="/dashboard/wallet" active-class="current">
             <a
-            class="
-              flex
-              items-center
-              px-4
-              py-2
-              mt-5
-              text-gray-600
-              transition-colors
-              duration-200
-              transform
-              dark:text-gray-400
-              hover:bg-yellow-400
-              dark:hover:bg-gray-700
-              dark:hover:text-gray-200
-              hover:text-white
-            "
-           
-          >
-            <svg
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+              class="
+                flex
+                items-center
+                px-4
+                py-2
+                mt-5
+                text-gray-600
+                transition-colors
+                duration-200
+                transform
+                dark:text-gray-400
+                hover:bg-yellow-400
+                dark:hover:bg-gray-700
+                dark:hover:text-gray-200
+                hover:text-white
+              "
             >
-              <path
-                d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+              <svg
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M15 5V7M15 11V13M15 17V19M5 5C3.89543 5 3 5.89543 3 7V10C4.10457 10 5 10.8954 5 12C5 13.1046 4.10457 14 3 14V17C3 18.1046 3.89543 19 5 19H19C20.1046 19 21 18.1046 21 17V14C19.8954 14 19 13.1046 19 12C19 10.8954 19.8954 10 21 10V7C21 5.89543 20.1046 5 19 5H5Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
 
-            <span class="mx-4 font-medium">Wallet</span>
-          </a>
-        </router-link>
-          <router-link to="/dashboard/history" active-class="current">
-
-          <a
-            class="
-              flex
-              items-center
-              px-4
-              py-2
-              mt-5
-              text-gray-600
-              transition-colors
-              duration-200
-              transform
-              dark:text-gray-400
-              hover:bg-yellow-400
-              dark:hover:bg-gray-700
-              dark:hover:text-gray-200
-              hover:text-white
-            "
-            href="#"
-          >
-           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
-</svg>
-
-            <span class="mx-4 font-medium">Transaction History</span>
-          </a>
+              <span class="mx-4 font-medium">Wallet</span>
+            </a>
           </router-link>
-         <router-link to="/dashboard/settings">
+          <router-link to="/dashboard/history" active-class="current">
             <a
-            class="
-              flex
-              items-center
-              px-4
-              py-2
-              mt-5
-              text-gray-600
-              transition-colors
-              duration-200
-              transform
-              dark:text-gray-400
-              hover:bg-yellow-400
-              dark:hover:bg-gray-700
-              dark:hover:text-gray-200
-              hover:text-white
-            "
-            href="#"
-          >
-            <svg
-              class="w-5 h-5"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+              class="
+                flex
+                items-center
+                px-4
+                py-2
+                mt-5
+                text-gray-600
+                transition-colors
+                duration-200
+                transform
+                dark:text-gray-400
+                hover:bg-yellow-400
+                dark:hover:bg-gray-700
+                dark:hover:text-gray-200
+                hover:text-white
+              "
+              href="#"
             >
-              <path
-                d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z"
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"
+                />
+              </svg>
 
-            <span class="mx-4 font-medium">Settings</span>
-          </a>
-         </router-link>
+              <span class="mx-4 font-medium">Transaction History</span>
+            </a>
+          </router-link>
+          <router-link to="/dashboard/settings">
+            <a
+              class="
+                flex
+                items-center
+                px-4
+                py-2
+                mt-5
+                text-gray-600
+                transition-colors
+                duration-200
+                transform
+                dark:text-gray-400
+                hover:bg-yellow-400
+                dark:hover:bg-gray-700
+                dark:hover:text-gray-200
+                hover:text-white
+              "
+              href="#"
+            >
+              <svg
+                class="w-5 h-5"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+
+              <span class="mx-4 font-medium">Settings</span>
+            </a>
+          </router-link>
           <a
             class="
               flex
@@ -261,8 +265,6 @@
               dark:hover:bg-gray-700
               dark:hover:text-gray-200
               hover:text-white
-              
-              
             "
             href="#"
           >
@@ -285,6 +287,7 @@
           </a>
         </nav>
       </div>
+
     </div>
   </div>
 </template>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,24 +1,45 @@
 <template>
   <div class="dashboard dark:bg-dark">
     <DashboardNav />
-    <div
-      class="
-        md:max-w-full
-        md:ml-0
-        2xl:ml-52
-        md:px-24
-        lg:px-16
-        2xl:container
-        lg:py-10
-        pt-12
-        text-white
-      "
-    >
-      <Sidebar />
-      <div class="grid grid-cols-6 gap-4 ml-0 lg:ml-64 xl:ml-80  ">
+    <div class="container px-6 py-4 mx-auto ">
+      <div class="flex flex-wrap overflow-hidden lg:-mx-4 xl:-mx-6">
+        <div
+          class="
+            w-full
+            overflow-hidden
+            lg:my-4
+            lg:px-4
+            lg:w-1/2
+            xl:my-6
+            xl:px-4
+            xl:w-3/12
+          "
+        >
+        <Sidebar/>
+        </div>
+
+        <div
+          class="
+            w-full
+            overflow-hidden
+            lg:my-4
+            lg:px-4
+            lg:w-1/2
+            xl:my-6
+            xl:px-6
+            xl:w-9/12
+          "
+        >
+          <div class="grid grid-cols-6 gap-4">
         <router-view></router-view>
       </div>
+        </div>
+      </div>
     </div>
+
+    <!-- <div class="grid grid-cols-6 gap-4 ml-0 lg:ml-64 xl:ml-80  ">
+        <router-view></router-view>
+      </div> -->
   </div>
 </template>
 <script>
@@ -26,9 +47,6 @@ import Sidebar from "../components/Sidebar.vue";
 import DashboardNav from "../components/DashboardNav.vue";
 export default {
   components: { Sidebar, DashboardNav },
-
-
-  
 };
 </script>
 
