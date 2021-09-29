4bebaa8e0c5d2b4ef2adca6b8e777106fad60bc5
khite-engineering <72549011+khite-engineering@users.noreply.github.com>
2021-09-29 23:11:10 +0100

    Create Users.js

---
+++ src/Models/Users.js
@@ -0,0 +1,35 @@
+
+
+class Users {
+  
+    
+    Constructor(userName, _name, emailAddress, dateCreated){
+        this.userName = userName;
+        this._name = _name;
+        this._emailAddress = emailAddress;
+        this.dateCreated = dateCreated;
+    }
+
+    accountTime() {
+        let date = new Date();
+        return date.getFullYear() - this.dateCreated;
+    }
+
+    dashboardCredentials(){
+        return (this.userName, this._name, this._emailAddress)
+    }
+
+    get getUserName(){
+        return this.userName;
+    }
+
+    get getEmailAddress(){
+        return this._emailAddress;
+    }
+
+    get getName(){
+        return this._name;
+    }
+
+    
+}
